49095b4e1e96e037c0648536e970ecefc677ce05
JyYoungK <junyoung@my.yorku.ca>
2022-12-14 16:26:23 -0500

    Minor Fix

--- client/src/components/Music/MusicPlayer/index.jsx
+++ client/src/components/Music/MusicPlayer/index.jsx
@@ -26,6 +26,7 @@ const MusicPlayer = () => {
   const [repeat, setRepeat] = useState(false);
   const [shuffle, setShuffle] = useState(false);
   const dispatch = useDispatch();
+
   useEffect(() => {
     if (currentSongs?.length) dispatch(playPause(true));
   }, [currentIndex]);
