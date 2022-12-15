e4b839bba09dda5be821c477b02b3c0fb0aeffcd
JyYoungK <junyoung@my.yorku.ca>
2022-12-15 12:21:23 -0500

    Update unsplashCore.js

--- client/src/utils/redux/services/unsplashCore.js
+++ client/src/utils/redux/services/unsplashCore.js
@@ -18,15 +18,6 @@ export const unsplashCoreApi = createApi({
     getRandomPhotos: builder.query({
       query: () => `photos/random?count=20`,
     }),
-    getNaturePhotos: builder.query({
-      query: () => `search/photos?query=nature?per_page=20`,
-    }),
-    getPuppyPhotos: builder.query({
-      query: () => `search/photos?query=puppy?per_page=20`,
-    }),
-    getCatPhotos: builder.query({
-      query: () => `search/photos?query=kitten?per_page=20`,
-    }),
     getPicturesBySearch: builder.query({
       query: (search) => `search/photos?query=${search}?per_page=20`,
       //query: (search) => search will give ? instead of /
@@ -36,8 +27,5 @@ export const unsplashCoreApi = createApi({
 
 export const {
   useGetRandomPhotosQuery,
-  useGetNaturePhotosQuery,
-  useGetPuppyPhotosQuery,
-  useGetCatPhotosQuery,
   useGetPicturesBySearchQuery,
 } = unsplashCoreApi;
